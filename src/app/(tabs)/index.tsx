4dbda2a5045a81dc4dd5ec89eaa08d3cfa42fd1b
Ian Abreu <ianabreudev@gmail.com>
2025-02-18 04:10:46 -0300

    initial config

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
 import { Stack } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
 
-import { ScreenContent } from '~/components/ScreenContent';
+import { ScreenContent } from '@/components/ScreenContent';
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
+      <View className="bg-red-600" style={styles.container}>
         <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
       </View>
     </>
